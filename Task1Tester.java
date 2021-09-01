56f3cf939922f8f66d421f17930478b23dc5270f
Enes Akbulut <eenesakbulutt@gmail.com>
2021-09-01 17:09:38 -0400

    Hw17

---
+++ Task1Tester.java
@@ -0,0 +1,22 @@
+package com.syntax.class17;
+
+public class Task1Tester {
+    public static void main(String[] args) {
+
+
+        double[]array={10,20,30,40};
+
+        Task1 task1 =new Task1();
+        System.out.println(task1.sumArray(array));
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Task2Tester.java
@@ -0,0 +1,17 @@
+package com.syntax.class17;
+
+public class Task2Tester {
+    public static void main(String[] args) {
+
+//   Task2 task2 =new Task2();
+//  System.out.println(task2.reverse("Sunday"));
+
+        System.out.println(new Task2().reverse("Sunday"));
+
+
+
+
+    }
+
+
+}
